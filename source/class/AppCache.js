1fa508e909fd773d33d87c6874fb810a6111656b
Sebastian Fastner <mail@sebastianfastner.de>
2012-09-05 17:05:59 +0200

    Change from spaces to tabs

--- source/class/AppCache.js
+++ source/class/AppCache.js
@@ -21,66 +21,66 @@
  
 (function(global) {
 
-  var appCache = global.applicationCache;
-  
-  core.Module("appcache.AppCache", {
-    /**
-     * {Boolean} Start application caching in browser, returns <code>false</code> if app cache is not supported.
-     */
-    start : function(baseURL) {
-      if (!!appCache) {
-        var iframe = document.createElement("iframe");
-        core.bom.Style.set(iframe, {
-          position: "absolute",
-          top: "-5000px",
-          width: "1px",
-          height: "1px"
-        });
+	var appCache = global.applicationCache;
+	
+	core.Module("appcache.AppCache", {
+		/**
+		 * {Boolean} Start application caching in browser, returns <code>false</code> if app cache is not supported.
+		 */
+		start : function(baseURL) {
+			if (!!appCache) {
+				var iframe = document.createElement("iframe");
+				core.bom.Style.set(iframe, {
+					position: "absolute",
+					top: "-5000px",
+					width: "1px",
+					height: "1px"
+				});
 
-        var src = baseURL ? baseURL : '';
-        src += 'index-' + core.Env.CHECKSUM + '.html';
-        iframe.src = src;
-        
-        document.body.appendChild(iframe);
-        
-        return true;
-      } else {
-        return false;
-      }
-    },
-    
-    /**
-     * {String|null} Return application cache status or null if not supported
-     */
-    status : function() {
-      if (!appCache) {
-        return null;
-      }
-      
-      switch (appCache.status) {
-        case appCache.UNCACHED: // UNCACHED == 0
-          return 'UNCACHED';
-          break;
-        case appCache.IDLE: // IDLE == 1
-          return 'IDLE';
-          break;
-        case appCache.CHECKING: // CHECKING == 2
-          return 'CHECKING';
-          break;
-        case appCache.DOWNLOADING: // DOWNLOADING == 3
-          return 'DOWNLOADING';
-          break;
-        case appCache.UPDATEREADY:  // UPDATEREADY == 4
-          return 'UPDATEREADY';
-          break;
-        case appCache.OBSOLETE: // OBSOLETE == 5
-          return 'OBSOLETE';
-          break;
-        default:
-          return 'UKNOWN CACHE STATUS';
-          break;
-      };
-    }
-  });
+				var src = baseURL ? baseURL : '';
+				src += 'index-' + core.Env.CHECKSUM + '.html';
+				iframe.src = src;
+				
+				document.body.appendChild(iframe);
+				
+				return true;
+			} else {
+				return false;
+			}
+		},
+		
+		/**
+		 * {String|null} Return application cache status or null if not supported
+		 */
+		status : function() {
+			if (!appCache) {
+				return null;
+			}
+			
+			switch (appCache.status) {
+				case appCache.UNCACHED: // UNCACHED == 0
+					return 'UNCACHED';
+					break;
+				case appCache.IDLE: // IDLE == 1
+					return 'IDLE';
+					break;
+				case appCache.CHECKING: // CHECKING == 2
+					return 'CHECKING';
+					break;
+				case appCache.DOWNLOADING: // DOWNLOADING == 3
+					return 'DOWNLOADING';
+					break;
+				case appCache.UPDATEREADY:  // UPDATEREADY == 4
+					return 'UPDATEREADY';
+					break;
+				case appCache.OBSOLETE: // OBSOLETE == 5
+					return 'OBSOLETE';
+					break;
+				default:
+					return 'UKNOWN CACHE STATUS';
+					break;
+			};
+		}
+	});
 
 })(this);
